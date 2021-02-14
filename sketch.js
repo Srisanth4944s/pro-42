f5b9de202554cfc7f8b5823f9a7d88b80e6f77fb
Srisanth4944s <70645902+Srisanth4944s@users.noreply.github.com>
2021-02-14 18:49:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+
+var hour,minute,second
+var HAG,MINSGS,SECAG
+
+function setup() {
+  createCanvas(800,400);
+  angleMode(DEGREES);
+
+}
+
+function draw() {
+  background(255,255,255);  
+  translate(200,200)
+rotate(-90)
+  drawSprites();
+  
+  hr = hour()
+  min = minute()
+  sec = second()
+  
+  HAG = map(hr%12,0,12,0,360)
+  MINSGS = map(min,0,60,0,360)
+  SECAG = map(sec,0,60,0,360)
+
+  push()
+  rotate(HAG)
+  stroke(0,0,255)
+  strokeWeight(7)
+  line(0,0,50,0)
+  pop()
+
+  push()
+  rotate(MINSGS)
+  stroke(0,255,0)
+  strokeWeight(7)
+  line(0,0,50,0)
+  pop()
+
+  push()
+  rotate(SECAG)
+  stroke(255,0,0)
+  strokeWeight(7)
+  line(0,0,50,0)
+  pop()
+
+  stroke(255,0,255)
+  point(0,0)
+
+
+strokeWeight(10)
+noFill()
+stroke(255,0,0)
+arc(0,0,300,300,0,SECAG)
+stroke(0,255,0)
+arc(0,0,280,280,0,MINSGS)
+stroke(0,0,255)
+arc(0,0,260,260,0,HAG)
+}
